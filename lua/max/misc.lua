82ca403047bd841931a7f0259df5ad8d87d0d97a
max <30047099+mxtw@users.noreply.github.com>
2023-02-19 18:11:12 +0100

    org config

--- lua/max/misc.lua
+++ lua/max/misc.lua
@@ -4,7 +4,16 @@ require("nvim-autopairs").setup()
 -- org
 require("orgmode").setup({
     org_agenda_files = { "~/org/**/*" },
+    org_default_notes_file = "~/org/notes.org",
     org_hide_leading_stars = true,
+    org_todo_keywords = {
+        "TODO",
+        "WAIT",
+        "|",
+        "DONE",
+        "DELEGATED",
+    },
+    org_hide_emphasis_markers = true,
 })
 
 require("orgmode").setup_ts_grammar()
