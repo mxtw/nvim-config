d4e4b5bb58c66b51880318fab08e75cc370c30ee
max <30047099+mxtw@users.noreply.github.com>
2023-11-04 18:23:11 +0100

    remove ibl scope again

--- lua/max/visual.lua
+++ lua/max/visual.lua
@@ -50,10 +50,7 @@ require("org-bullets").setup()
 
 require("ibl").setup({
     scope = {
-        enabled = true,
-        show_start = false,
-        show_end = false,
-        injected_languages = true,
+        enabled = false,
     },
 })
 require("colorizer").setup()
