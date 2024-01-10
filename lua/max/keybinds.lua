cc2c7b45b21cf1f807541d34d69b323f8cb4d5c2
max <30047099+mxtw@users.noreply.github.com>
2024-01-10 19:19:00 +0100

    move line keybinds
    stolen from primeagen

--- lua/max/keybinds.lua
+++ lua/max/keybinds.lua
@@ -16,3 +16,10 @@ vim.keymap.set("n", "<leader>wk", "<cmd>resize +5<cr>")
 vim.keymap.set("n", "<leader>wl", "<cmd>vertical resize +10<cr>")
 vim.keymap.set("n", "<leader>ws", "<cmd>split<cr>")
 vim.keymap.set("n", "<leader>wv", "<cmd>vsplit<cr>")
+
+-- these move the selected line(s) up or down, stolen from theprimeagen
+-- https://github.com/ThePrimeagen/init.lua/blob/97c039bb88d8bbbcc9b1e3d0dc716a2ba202c6d2/lua/theprimeagen/remap.lua#L5-L6
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "<S-Down>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<S-Up>", ":m '<-2<CR>gv=gv")
