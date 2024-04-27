4d32e36959187676fb088f3a5b127700c5d94de5
max <30047099+mxtw@users.noreply.github.com>
2024-04-27 18:48:11 +0200

    fix: nvim-dap-ui new dependency

--- lua/max/plugins/debugger.lua
+++ lua/max/plugins/debugger.lua
@@ -4,6 +4,7 @@ return {
         dependencies = {
             { "theHamsta/nvim-dap-virtual-text", config = true },
             { "rcarriga/nvim-dap-ui" },
+            { "nvim-neotest/nvim-nio" },
         },
         config = function()
             local dap = require("dap")
