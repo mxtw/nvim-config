ba5bbbb7c8ac728ffe8da5e756cc3487914b3cb8
max <30047099+mxtw@users.noreply.github.com>
2024-07-09 21:53:42 +0200

    feat(lsp, visual): noice markdown stuff

--- lua/max/plugins/visual.lua
+++ lua/max/plugins/visual.lua
@@ -37,11 +37,11 @@ return {
         opts = {
             lsp = {
                 -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-                -- override = {
-                --     ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                --     ["vim.lsp.util.stylize_markdown"] = true,
-                --     ["cmp.entry.get_documentation"] = true,
-                -- },
+                override = {
+                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                    ["vim.lsp.util.stylize_markdown"] = true,
+                    ["cmp.entry.get_documentation"] = true,
+                },
                 hover = {
                     enabled = false
                 },
