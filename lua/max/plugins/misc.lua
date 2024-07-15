e88b17e1923b3afd119e59d316321c79cc843fad
max <30047099+mxtw@users.noreply.github.com>
2024-07-15 18:47:06 +0200

    feat(dadbod): add dadbod + dadbod-ui

--- lua/max/plugins/misc.lua
+++ lua/max/plugins/misc.lua
@@ -118,5 +118,29 @@ return {
                 desc = "Toggle Post Write"
             },
         }
+    },
+
+    {
+        'kristijanhusak/vim-dadbod-ui',
+        dependencies = {
+            { 'tpope/vim-dadbod',                     lazy = true },
+            { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
+        },
+        cmd = {
+            'DBUI',
+            'DBUIToggle',
+            'DBUIAddConnection',
+            'DBUIFindBuffer',
+        },
+        keys = {
+            {
+                "<leader>Dd",
+                "<cmd>DBUIToggle<cr>",
+                desc = "Toggle DBUI"
+            }
+        },
+        init = function()
+            vim.g.db_ui_use_nerd_fonts = 1
+        end,
     }
 }
