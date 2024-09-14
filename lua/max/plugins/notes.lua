660779a4d7df9cd9cb0f00a3d9764b7589db5439
max <max@macks.cloud>
2024-09-14 18:47:04 +0200

    ref(notes): unnecessary braces

--- lua/max/plugins/notes.lua
+++ lua/max/plugins/notes.lua
@@ -1,12 +1,10 @@
 return {
 
     {
-        {
-            'MeanderingProgrammer/render-markdown.nvim',
-            ft = "markdown",
-            opts = {},
-            dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-        }
+        'MeanderingProgrammer/render-markdown.nvim',
+        ft = "markdown",
+        opts = {},
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     },
 
     {
